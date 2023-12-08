a3bb6ff17651df05984dbabc2baca61f4189678e
Shigenobu Furuta <shigenobu@walksocket.com>
2023-12-08 22:44:04 +0900

    Version 0.1.1.
    * Respond for .NET8.

--- PurpleSofa/PsSessionManager.cs
+++ PurpleSofa/PsSessionManager.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Net.Sockets;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace PurpleSofa;
 
